78a019fbdd4fae8d887e01b775828bb975121c6e
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2015-12-16 22:11:22 +0100

    Add Users pnp4nagios template

---
+++ templates/check_mk-users.php
@@ -0,0 +1,9 @@
+<?php
+
+$def[1] .= "AREA:var1#00FF00:\"Response Times \" " ;
+$def[1] .= "LINE1:var1#000000 " ;
+$def[1] .= "GPRINT:var1:LAST:\"%3.4lg %s$UNIT[1] LAST \" ";
+$def[1] .= "GPRINT:var1:MAX:\"%3.4lg %s$UNIT[1] MAX \" ";
+$def[1] .= "GPRINT:var1:AVERAGE:\"%3.4lg %s$UNIT[1] AVERAGE \" ";
+
+?>
